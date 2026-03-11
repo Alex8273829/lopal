ee7a213003cf631b34f7227a4e9e09530e129408
Alex8273829 <alerib.alcazar.perez@gmail.com>
2026-03-11 12:40:45 -0300

    feat: deteção de operador inválido

--- calculadora/calculadora.js
+++ calculadora/calculadora.js
@@ -1,39 +1,43 @@
 function calculadora(){
-    while (true) {
-        let resultado = 0.0;
-        let operando1;
-        let operando2;
-        let operador;
-        let continuar;
+        while (true) {
+            let resultado = 0.0;
+            let operando1;
+            let operando2;
+            let operador;
+            let continuar;
+            let operador_valido = true;
 
-        operando1 = parseFloat(prompt("digite o primeiro numero"));
-        operando2 = parseFloat(prompt("digite o segundo numero"));
-        operador = prompt("digite uma das operações: +, -, *, /");
+            operando1 = parseFloat(prompt("digite o primeiro numero"));
+            operando2 = parseFloat(prompt("digite o segundo numero"));
+            operador = prompt("digite uma das operações: +, -, *, /");
 
-        if (operador == "+") {
-            resultado = operando1 + operando2;
-        } else if (operador === "-") {
-            resultado = operando1 - operando2;
-        } else if (operador === "*") {
-            resultado = operando1 * operando2;
-        } else if (operador === "/") {
-            resultado = operando1 / operando2
-            if (operando2 === 0) {
-                alert("divisão por zero não é permitida");
-                alert("quer continuar?");
+            if (operador == "+") {
+                resultado = operando1 + operando2;
+            } else if (operador === "-") {
+                resultado = operando1 - operando2;
+            } else if (operador === "*") {
+                resultado = operando1 * operando2;
+            } else if (operador === "/") {
+                resultado = operando1 / operando2
+                if (operando2 === 0) {
+                    alert("divisão por zero não é permitida");
+                    continuar = prompt("digite 'sim' para continuar ou 'não' para sair");
+                    if(continuar === "não"){
+                        return
+                    }
+                }  else{
+                resultado = operando1 / operando2;
+                }
+            } else {
+                alert("operador inválido");
+                operador_valido = false;
                 continua = prompt("digite 'sim' para continuar ou 'não' para sair");
                 if( continua === "não"){
-                    return"
-                }
-            }  else{
-            resultado = operando1 / operando2;
+                return;
             }
-        } else {
-            alert("operador inválido");
-            return;
-        }
-        if (operador != "/" || operando2 != 0){
-            alert("resultado: " + operando1 + " " + operador + " " + operando2 + " = " + resultado);
+            if ((operador != "/" || operando2 != 0) && operador_valido != false) {
+                alert("resultado: " + operando1 + " " + operador + " " + operando2 + " = " + resultado);
+            }
         }
     }
-}
+}      
